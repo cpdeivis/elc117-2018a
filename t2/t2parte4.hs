7fc7aa193bc7a5a11170736ee47c1c427da26f61
Deivis Pereira <dcpereira@inf.ufsm.br>
2018-03-25 19:59:42 -0300

    t2parte4 finalizado, eu acho =/

--- t2/t2parte4.hs
+++ t2/t2parte4.hs
@@ -8,6 +8,7 @@ decodeChar :: Int -> Char
 decodeChar n = chr (ord 'a' + n)
 -- Calcula percentagem: n/m*100
 percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m)*100
 -- Rotacao de uma lista para esquerda em n posicoes
 rotate :: Int -> [a] -> [a]
 rotate n xs = drop n xs ++ take n xs
@@ -25,3 +26,32 @@ crack cs = encodeStr cs (-factor)
            where factor = head (positions (minimum chitab) chitab)
                  chitab = [ chi2 (rotate n table' ) table | n <- [0..25] ]
                  table' = freqs cs
+
+-- Aqui Começa as Outras Implementações
+-- Exercicio 1
+shiftChar :: Char -> Int -> Char
+shiftChar ch n
+  | (encodeChar ch) > 25 || (encodeChar ch) < 0 = ch
+  | n >= 25 =  shiftChar ch (n-25)
+  | ((encodeChar ch) + n) > 25 = shiftChar 'a' (n-1)
+  | otherwise = decodeChar ((encodeChar ch) + n)
+
+-- Exercicio 2
+encodeStr :: String -> Int -> String
+encodeStr str n = map (\x -> shiftChar x n) str
+
+-- Exercicio 3
+countValids :: String -> Int
+countValids str = length(filter (\x -> ord x > 96 && ord x < 123) str)
+
+-- Exercicio 4
+countChar :: Char -> String -> Int
+countChar ch str = length(filter (\x -> ord x == ord ch) str)
+
+-- Exercicio 5
+freqs :: String -> [Float]
+freqs str = [ percent (countChar x str) y | x <- ['a'..'z'], y <- [countValids str]]
+
+-- Exercicio 6
+positions :: Float -> [Float] -> [Int]
+positions fl lis = map (\x -> snd x) (filter (\x -> (fst x) == fl) (zip lis [0..(length lis - 1)]))
