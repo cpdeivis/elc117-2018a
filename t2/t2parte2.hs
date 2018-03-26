cc694f7d40a0ced0e6f6dba2c52ac6bd46d1f0a1
Deivis Pereira <dcpereira@inf.ufsm.br>
2018-03-25 22:39:22 -0300

    "Feitooooooooooooo" - BRITO, Paulo

--- t2/t2parte2.hs
+++ t2/t2parte2.hs
@@ -12,18 +12,29 @@ isBin' :: String -> Bool
 isBin' "" = False
 isBin' str = length(filter (\x -> x /= '0' && x /= '1') str) == 0
 
--- Exercicio 3 = Não Funciona Ainda
+-- Exercicio 3
 auxBin2Dec :: [Int] -> Int -> Int
 auxBin2Dec [] n = 0
-auxBin2Dec bits n = (((head bits) * 2)^n) + (auxBin2Dec (tail bits) ((length bits)-1))
+auxBin2Dec [0] n = 0
+auxBin2Dec bits n = (((head bits) * 2)^n) + (auxBin2Dec (tail bits) (n-1))
 
 bin2dec :: [Int] -> Int
 bin2dec [] = undefined
 bin2dec bits = auxBin2Dec bits ((length bits)-1)
 
 -- Exercicio 4
+bin2dec' :: [Int] -> Int
+bin2dec' [] = undefined
+bin2dec' bits = sum(map (\x -> if (fst x) == 1 then ((fst x)*2)^(snd x) else 0) (zip (reverse bits) [0..]))
 
 -- Exercicio 5
+dec2bin' :: Int -> [Int]
+dec2bin' 0 = []
+dec2bin' n = (mod n 2) : dec2bin' (div n 2)
+
+dec2bin :: Int -> [Int]
+dec2bin 0 = [0]
+dec2bin n = reverse (dec2bin' n)
 
 -- Exercicio 6
 isHex :: String -> Bool
